d16476aec96959c8938ae2d3d2de2931e98afc70
shivrjpatil <shivrajp972@gmail.com>
2022-12-10 10:53:12 +0530

    [shiv]added array program

---
+++ Practice/Array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Array
+    {
+        public void NewArray()
+        {
+            Console.WriteLine("Please enter the size of the array");
+            int size=Convert.ToInt32(Console.ReadLine());
+            int[] num = new int[size];
+            Console.WriteLine("enter the data");
+            for (int i= 0; i < num.Length; i++)
+            {
+                num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("array elements");
+            for (int i= 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i] + "");
+                Console.ReadLine();
+            }
+        }
+    }
+}
